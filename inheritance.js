b50185ab8f0ab3c9a6e264a59183af96486b13ee
birbwatcher <d@rkmind.cc>
2022-09-16 16:26:56 +0200

    feat: es6 plus method implemented

--- inheritance.js
+++ inheritance.js
@@ -2,4 +2,19 @@ class MainBuilder {
     constructor(value) {
         this.value = value;
     }
+
+    get() {
+        return this.value;
+    }
 }
+
+//ES6
+
+class IntBuilder extends MainBuilder {
+    plus(...args) {
+        [...args].forEach(item => this.value += item)
+        return this
+    }
+}
+
+let meow = new IntBuilder(6);
